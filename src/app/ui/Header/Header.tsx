22a8213187557f1e405461b60851d0b76f1294cd
Egor <egor.lapin.workspace@gmail.com>
2023-06-26 15:35:41 +0300

    сделано навигационное меню, но не адаптировано

--- src/app/ui/Header/Header.tsx
+++ src/app/ui/Header/Header.tsx
@@ -1,5 +1,5 @@
 import cn from 'classnames';
-import { FC, useState } from "react";
+import { FC, useRef, useEffect, useState } from "react";
 
 import styles from "./Header.module.scss";
 import {
@@ -10,6 +10,8 @@ import {
 
 import { NavSection } from '../NavSection';
 
+const MIN_SCROLL_FOR_FIXED_HEADER = 100;
+
 interface Props {
   header: Array<({ title: string })>;
   tel: string;
@@ -18,11 +20,41 @@ interface Props {
 export const Header: FC<Props> = ({ tel, header }) => {
 
   const [opened, setOpened] = useState(false);
+  const [fixed, setFixed] = useState(true);
+  const [isOnTop, setIsOnTop] = useState(true);
 
+  const oldScroll = useRef(0);
   const closeMenu = () => setOpened(false);
 
+  const onScroll = () => {
+    const scrollTop = window.scrollY;
+
+    if (opened) {
+      oldScroll.current = scrollTop;
+      return;
+    }
+
+    if (oldScroll.current > scrollTop || scrollTop < MIN_SCROLL_FOR_FIXED_HEADER) {
+      setFixed(true);
+    } else {
+      setFixed(false);
+    }
+
+    setIsOnTop(scrollTop < MIN_SCROLL_FOR_FIXED_HEADER);
+
+    oldScroll.current = scrollTop;
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, [opened]);
+
   return (
-    <header className={styles.root}>
+    <header className={cn(styles.root, fixed && styles.fixed, !isOnTop && styles.darkened)}>
       <div className={styles.wrap}>
         <Logo className={styles.logo} />
         <div className={styles.right}>
